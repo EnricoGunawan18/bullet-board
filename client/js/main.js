dbc1a5d42ace4ca81157e488e03632e3ea6dfd9f
EnricoGunawan18 <[20J70008eg@edu.tca.ac.jp]>
2021-11-10 15:11:36 +0900

    c

--- client/js/main.js
+++ client/js/main.js
@@ -1,5 +1,10 @@
 function post(){
-    alert("post");
+    //var request = new XMLHttpRequest();
+    //var url = 'http://localhost:8080/api';
+
+    var name = document.getElementById('name1').value;
+    var message = document.getElementById('message1').value;
+    alert(name);
 }
 function edit(){
     alert("edit");
